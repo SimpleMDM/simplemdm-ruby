3bd111c96e9fe466d3f1bd5ef5c2ae4f9513a04d
Taylor Boyko <taylorboyko@gmail.com>
2016-08-03 10:52:53 -0700

    added support for moving devices between device groups

--- lib/simplemdm/device_group.rb
+++ lib/simplemdm/device_group.rb
@@ -13,5 +13,18 @@ module SimpleMDM
       build hash['data']
     end
 
+    def add_device(device)
+      raise "The object you provided is not a device" unless app.kind_of?(SimpleMDM::Device)
+
+      hash, code = fetch("device_groups/#{self.id}/devices/#{device.id}", :post)
+
+      if code == 204
+        self['device_ids'] = self['device_ids'] | [device.id]
+        true
+      else
+        false
+      end
+    end
+
   end
 end
